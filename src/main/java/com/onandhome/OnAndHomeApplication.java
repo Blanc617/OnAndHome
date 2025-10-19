c04299f876ffa8dac00b367d0102f83af7a8a822
Blanc617 <jinusong14@gmail.com>
2025-10-19 20:04:22 +0900

    첫 커밋

---
+++ src/main/java/com/onandhome/OnAndHomeApplication.java
@@ -0,0 +1,13 @@
+package com.onandhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnAndHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnAndHomeApplication.class, args);
+	}
+
+}
