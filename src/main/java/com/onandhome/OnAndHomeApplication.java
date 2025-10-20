3f3d3c9ef157a52b27bb7b9f88b284c650fe32a7
DESKTOP-06AKLNP\user <rezner@naver.com>
2025-10-20 11:26:24 +0900

    no message

--- src/main/java/com/onandhome/OnAndHomeApplication.java
+++ src/main/java/com/onandhome/OnAndHomeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnAndHomeApplication {
+public class OnAndHomeApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnAndHomeApplication.class, args);
