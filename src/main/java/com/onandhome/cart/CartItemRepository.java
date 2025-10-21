d13919f72f63c6676fe6aaa5dd5ccddc0cf063f9
Blanc617 <jinusong14@gmail.com>
2025-10-21 14:34:56 +0900

    프로젝트 구조 변경 - jinu

--- src/main/java/com/onandhome/cart/CartItemRepository.java
+++ src/main/java/com/onandhome/cart/CartItemRepository.java
@@ -1,7 +1,7 @@
 package com.onandhome.cart;
 
 import com.onandhome.cart.entity.CartItem;
-import com.onandhome.product.entity.Product;
+import com.onandhome.admin.adminProduct.entity.Product;
 import com.onandhome.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
